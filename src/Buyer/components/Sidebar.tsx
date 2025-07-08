9c04e4ee918bc0d9623a259915afc8e8dfb5dccc
Sanjai Kannan G <136103561+sanjaikannang@users.noreply.github.com>
2025-07-08 19:53:27 +0530

    updated sidebar width in buyer portal

--- src/Buyer/components/Sidebar.tsx
+++ src/Buyer/components/Sidebar.tsx
@@ -45,7 +45,7 @@ const Sidebar: React.FC<SidebarProps> = ({ isSidebarOpen, setIsSidebarOpen }) =>
     return (
         <>
             {/* Sidebar */}
-            <aside className={`${isSidebarOpen ? 'translate-x-0' : '-translate-x-full'} lg:translate-x-0 fixed lg:static inset-y-0 left-0 z-50 w-64 bg-whiteColor shadow-lg transform transition-transform duration-300 ease-in-out flex flex-col`}>
+            <aside className={`${isSidebarOpen ? 'translate-x-0' : '-translate-x-full'} lg:translate-x-0 fixed lg:static inset-y-0 left-0 z-50 w-48 bg-whiteColor shadow-lg transform transition-transform duration-300 ease-in-out flex flex-col`}>
 
                 {/* Logo */}
                 <div className="h-16 flex items-center justify-center flex-shrink-0">
